b110769261ac91084c60a3cb2906e46b5376b398
darshithv <darshithv3392@gmail.com>
2023-07-15 22:57:57 +0530

    code is added

---
+++ problem_3.py
@@ -0,0 +1,19 @@
+#horizontal bar chart
+def hor_bar_chart(new_lt,n):
+    n = len(new_lt)
+    for i in range(0,length):
+        print(new_lt[i],end = '\t')
+        for j in range(1,new_lt[i] + 1):
+            print("*",end = '')    
+        print()    
+
+
+lt = []
+n = int(input("enter the size of element: "))
+print("enter the elements one by one")
+
+for i in range(0,n):
+    number = int(input("enter the elements: "))
+    lt.append(number)
+
+print(hor_bar_chart(lt,n))
