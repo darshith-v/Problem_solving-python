e03c7cb4ba94ee0a597952a445a860b31c7c4090
darshithv <darshithv3392@gmail.com>
2023-07-15 23:01:56 +0530

    changes done

--- problem_3.py
+++ problem_3.py
@@ -1,7 +1,7 @@
 #horizontal bar chart
 def hor_bar_chart(new_lt,n):
     n = len(new_lt)
-    for i in range(0,length):
+    for i in range(0,n):
         print(new_lt[i],end = '\t')
         for j in range(1,new_lt[i] + 1):
             print("*",end = '')    
