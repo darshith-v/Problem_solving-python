b036192553dd4f849d5d39da8b5cc13a399b77d3
darshithv <darshithv3392@gmail.com>
2023-08-12 09:36:55 +0530

    solved another problem, merge sort.

--- merge_sort.py
+++ merge_sort.py
@@ -1,41 +1,39 @@
 def merge_sort(arr):
-    if len(arr) > 1:
+    if len(arr) <= 1:
+        return arr
+    
+    #divide the array
+    mid_idx = len(arr)//2
+    left_arr = arr[:mid_idx]
+    right_arr = arr[mid_idx:]
 
-        #Divide the array into two halves
-        mid = len(arr)//2
-        left_arr = arr[:mid]
-        right_arr = arr[mid:]
+    #recursive for divided array
+    left_arr = merge_sort(left_arr)
+    right_arr = merge_sort(right_arr)
 
-        #recursion sort both halves
-        merge_sort(left_arr)
-        merge_sort(right_arr)
+    return merge(left_arr,right_arr)
 
-        i,j,k = 0, 0, 0
-        while i < len(left_arr) and j < len(right_arr):
-            if left_arr[i] < right_arr[j]:
-                arr[k] = left_arr[i]
-                i+=1
-                k+=1
-            else:
-                arr[k] = right_arr[j]
-                j+=1
-                k+=1
-        while i < len(left_arr):
-            arr[k] = left_arr[i]
+def merge(left,right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
             i+=1
-            k+=1
-        while j < len(right_arr):
-            arr[k] = right_arr[j]
+        else:
+            result.append(right[j])  
             j+=1
-            k+=1
-    ''' 
-    #if last element remains to add for arr[k]
-    arr[k].extend(arr[left_arr]:)
-    arr[k].extend(arr[right_arr]:)
-    '''
 
-n = int(input("enter n : "))
-lt = [int(input("enter the element one by one: ")) for i in range(n)]
+    result.extend(left[i:])      
+    result.extend(right[j:])
+
+    return result
+
+
+# n = int(input("enter n : "))
+# lt = [int(input("enter the element one by one: ")) for i in range(n)]
+
+lt = [45,89,66,23,90,21]
 
 result = merge_sort(lt)
 print(result)
