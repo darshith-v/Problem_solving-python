458ce9ad06694bca643329935065b5c5c35cadbe
darshithv <darshithv3392@gmail.com>
2024-03-30 23:47:12 +0530

    perfect number in python

---
+++ Revision/b contest.py
@@ -0,0 +1,35 @@
+# def coins():
+#     t = int(input())
+
+#     for _ in range(t):
+#         alice, bob = map(int, input().split())
+#         maxi = max(alice,bob)
+#         for i in range(maxi):
+
+#             if (alice == 1000000000 and bob == 1000000000):
+#                 break
+
+#             if alice >= bob:
+#                 alice -= 1
+#             else:
+#                 alice, bob = bob, alice
+#                 alice -= 1
+
+#             if bob >= alice:
+#                 bob -= 1
+#             else:
+#                 alice, bob = bob, alice
+#                 bob -= 1
+
+#         print(f'{"Alice" if alice > bob else "Bob"}')
+
+# coins()
+
+count = 0
+for i in range(1,100):
+  for j in range(1,i):
+    if (i % j == 0):
+      count += j
+  if(count==i):
+    print(count)
+  count = 0    
